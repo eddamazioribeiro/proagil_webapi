673c2e1ee1a0aed6ae87cd30989dd55781efaef1
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-31 13:15:10 -0300

    Retrieving data from database on main controller from using DataContext.

--- Source/Controllers/EventoController.cs
+++ Source/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Data;
 using ProAgil.WebAPI.Model;
 
 namespace ProAgil.WebAPI.Controllers
@@ -12,43 +13,26 @@ namespace ProAgil.WebAPI.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private static readonly Evento[] Eventos =  {
-                new Evento() {
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Local = "Ribeirão Preto",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(10).ToString("dd/MM/yyyy")
-                },
-                new Evento() {
-                    EventoId = 2,
-                    Tema = "Node.js",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdPessoas = 150,
-                    DataEvento = DateTime.Now.AddDays(11).ToString("dd/MM/yyyy")
-                } 
-        };
+        public readonly DataContext _context;
+        public readonly IEnumerable<Evento> _eventos;
 
-        private readonly ILogger<EventoController> _logger;
-
-        public EventoController(ILogger<EventoController> logger)
+        public EventoController(DataContext context)
         {
-            _logger = logger;
+            _context = context;
+            _eventos = _context.Eventos.ToList();
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return Eventos.ToList();
+            return _eventos.ToList();
         }
 
         [HttpGet("{id}")]
         public Evento GetEventoById(string id)
         {
             int.TryParse(id, out int EventoId);
-            return Eventos.FirstOrDefault(x => x.EventoId == EventoId);
-        } 
+            return _eventos.FirstOrDefault(x => x.EventoId == EventoId);
+        }
     }
 }
